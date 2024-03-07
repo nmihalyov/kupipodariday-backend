938db37792a420dc14e445f783319c123798dcb0
nmihalyov <nikita.mihalyov@gmail.com>
2024-03-07 20:28:10 +0300

    Environment configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import configuration from './configuration';
 import { Offer } from './modules/offers/entities/offer.entity';
 import { OffersModule } from './modules/offers/offers.module';
 import { User } from './modules/users/entities/user.entity';
@@ -9,17 +12,26 @@ import { WishesModule } from './modules/wishes/wishes.module';
 import { Wishlist } from './modules/wishlists/entities/wishlist.entity';
 import { WishlistsModule } from './modules/wishlists/wishlists.module';
 
+config({ path: `.env${process.env.NODE_ENV === 'dev' ? '.dev' : ''}` });
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'kpd_db',
-      entities: [User, Wish, Offer, Wishlist],
-      synchronize: true,
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService) => ({
+        type: configService.get('database.type'),
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.database'),
+        entities: [User, Wish, Offer, Wishlist],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
     WishesModule,
